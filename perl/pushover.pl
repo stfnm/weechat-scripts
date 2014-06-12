4ada8148577b17f39267901beff60afe6854d332
stfn <stfnmd@gmail.com>
2014-06-12 11:08:37 +0200

    pushover.pl 0.8

--- perl/pushover.pl
+++ perl/pushover.pl
@@ -23,7 +23,7 @@ use CGI;
 my %SCRIPT = (
 	name => 'pushover',
 	author => 'stfn <stfnmd@gmail.com>',
-	version => '0.7',
+	version => '0.8',
 	license => 'GPL3',
 	desc => 'Send push notifications to your mobile devices using Pushover, NMA or Pushbullet',
 	opt => 'plugins.var.perl',
